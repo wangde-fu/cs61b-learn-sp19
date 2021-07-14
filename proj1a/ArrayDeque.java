146bb0f248676cc24733f229d7a6a41fa89f5b65
wangde-fu <zero.p@111.com>
2021-07-14 19:42:19 +0800

    t

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,4 @@
-public class ArrayDeque{
+public class ArrayDeque<T>{
     private T[] items;
     private int nextFirst;//下次在头部增加时的索引
     private int nextLast;//下次在尾部增加时的索引
@@ -11,29 +11,97 @@ public class ArrayDeque{
         size=0;
     }
 
-    public int size() {return size;}
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int plusOne(int index) {
+        return (index + 1) % items.length;
+    }
+    public int minusOne(int index) {
+        return (index - 1 + items.length) % items.length;
+    }
 
-    public boolean isEmpty() {return size == 0;}
-    public int plusOne(int index) {return (index+1)% items.length;}
-    public int minusOne(int index) {return (index-1+items.length)% items.length;}
     public void resize(){
+        // 判断是否需要改变数组大小
+        int newsize;
         if(size==items.length){
             // 扩大数组
-            int newsize = items.length*2;
+            newsize = items.length*2;
         }
         else if(size<=(items.length/3)&&items.length>8){
             // 缩小数组
-            int newsize= items.length/2;
+            newsize= items.length/2;
         }
         else{return;}
 
-        @TODO
-        items=(T[]) new Object[newsize];
+        int theFirst=plusOne(nextFirst);
+        T[] newitems=(T[]) new Object[newsize];
+        for (int i=0; i<size; i++) {
+            newitems[i]=items[theFirst];
+            theFirst=plusOne(theFirst);
+        }
+        items=newitems;
         nextFirst=newsize-1;
-        nextLast=1;
+        nextLast=size;
     }
 
-    public void addFirst(T item){};
+    public T get(int index){
+        // 获取第index的项目
+        if (index>size) return null;
 
-    public void addLast(T item){};
+        int theFirst=plusOne(nextFirst);
+        return items[(index+theFirst)%items.length];
+    }
+
+    public void addFirst(T item){
+        resize();
+        items[nextFirst]=item;
+        nextFirst=minusOne(nextFirst);
+        size++;
+    };
+
+    public void addLast(T item){
+        resize();
+        items[nextLast]=item;
+        nextLast=plusOne(nextLast);
+        size++;
+    };
+
+    public void printDeque(){
+        // 打印所有项目
+        if (size==0) {
+            System.out.println("");
+            return;
+        }
+
+        int theFirst=plusOne(nextFirst);
+        for (int i=1; i<size; i++) {
+            System.out.print(items[theFirst]+" ");
+            theFirst=plusOne(theFirst);
+        }
+        System.out.println(items[minusOne(nextLast)]);
+    };
+
+    public T removeFirst(){
+        resize();
+        if(size == 0)return null;
+        nextFirst=plusOne(nextFirst);
+        T theItem=items[nextFirst];
+        items[nextFirst] = null;
+        size--;
+        return theItem;
+    };
+
+    public T removeLast(){
+        resize();
+        if(size == 0)return null;
+        nextLast=minusOne(nextLast);
+        T theItem=items[nextLast];
+        items[nextLast] =null;
+        size--;
+        return theItem;
+    };
 }
