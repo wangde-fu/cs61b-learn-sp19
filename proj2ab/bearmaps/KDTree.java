c7619dfde73e44cd25578aad9014c6a8c2ab92ea
wangde-fu <zero.p@111.com>
2022-01-19 18:33:46 +0800

    t

--- proj2ab/bearmaps/KDTree.java
+++ proj2ab/bearmaps/KDTree.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.Collections;
 
 public class KDTree implements PointSet {
-    private static Double error = 0.00000001;
+    //divider is in X dim if splitDim is false
+    private static final boolean HORIZONTAL = false;
     private Node root;             // root of KDTree
 
     private class Node {
@@ -34,12 +35,14 @@ public class KDTree implements PointSet {
     // Returns the closest point to the inputted coordinates.
     public Point nearest(double x, double y){}
 
-    // public static void main(String[] args) {
-    //     Point p1 = new point(7, 2);
-    //     Point p2 = new point(9, 6);
-    //     Point p3 = new point(5, 4);
-    //     Point p4 = new point(4, 7);
-    //     Point p5 = new point(8, 1);
-    //     Point p6 = new point(2, 3);
-    // }
+    private void reSort(List<Point> points, boolean splitDim){}
+
+    public static void main(String[] args) {
+        Point p1 = new point(7, 2);
+        Point p2 = new point(9, 6);
+        Point p3 = new point(5, 4);
+        Point p4 = new point(4, 7);
+        Point p5 = new point(8, 1);
+        Point p6 = new point(2, 3);
+    }
 }
